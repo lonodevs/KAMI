233508daa199d2b3245fb2675b4cf35105669113
Alex <147999158+AklisTib@users.noreply.github.com>
2025-04-08 17:57:54 +0400

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -30,12 +30,21 @@ cat <<EOF /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
 EOF
 
-sed -i 's/#Port 22/Port 2024/Ig' /etc/openssh/sshd_config
-sed -i 's/#MaxAuthTries 6/MaxAuthTries 2/Ig' /etc/openssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/Ig' /etc/openssh/sshd_config
+
+CONFIG_FILE="/etc/ssh/sshd_config"  
+
+# Изменить SSH-порт с 22 на 2024  
+sed -i 's/^#Port 22$/Port 2024/' "$CONFIG_FILE"  
+
+# Уменьшить MaxAuthTries с 6 до 2  
+sed -i 's/^#MaxAuthTries 6$/MaxAuthTries 2/' "$CONFIG_FILE"  
+
+# Разрешить аутентификацию по паролю 
+sed -i 's/^#PasswordAuthentication yes$/PasswordAuthentication yes/' "$CONFIG_FILE"  
 
 touch /etc/openssh/bannermotd
 cat <<EOF /etc/openssh/bannermotd
+
 ----------------------
 Authorized access only
 ----------------------
