6c0f432ad536f7f29aadb86e97647ff0f4dcf908
Alex <147999158+AklisTib@users.noreply.github.com>
2025-04-08 19:32:50 +0400

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -30,9 +30,7 @@ cat <<EOF > /etc/net/ifaces/ens20/ipv4address
 172.16.5.1/28
 EOF
 
-
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.conf
-
 
 apt-get update && apt-get install -y firewalld 
 systemctl enable --now firewalld
@@ -44,38 +42,32 @@ firewall-cmd --reload
 systemctl restart firewalld
 systemctl restart network
 
-
-#Создание пользователя sshuser и настройка sshd конфига
+# Создание пользователя sshuser и настройка sshd конфига
 useradd sshuser -u 1010
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 
-touch /etc/sudoers
-cat <<EOF /etc/sudoers
+cat <<EOF > /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
 EOF
-
 
 CONFIG_FILE="/etc/ssh/sshd_config"  
 
 # Изменить SSH-порт с 22 на 2024  
-sed -i 's/^#Port 22$/Port 2024/' "$CONFIG_FILE"  
+sed -i 's|^#Port 22$|Port 2024|' "$CONFIG_FILE"  
 
 # Уменьшить MaxAuthTries с 6 до 2  
-sed -i 's/^#MaxAuthTries 6$/MaxAuthTries 2/' "$CONFIG_FILE"  
+sed -i 's|^#MaxAuthTries 6$|MaxAuthTries 2|' "$CONFIG_FILE"  
 
 # Разрешить аутентификацию по паролю 
-sed -i 's/^#PasswordAuthentication yes$/PasswordAuthentication yes/' "$CONFIG_FILE"  
+sed -i 's|^#PasswordAuthentication yes$|PasswordAuthentication yes|' "$CONFIG_FILE"  
 
-touch /etc/openssh/bannermotd
-cat <<EOF /etc/openssh/bannermotd
+cat <<EOF > /etc/ssh/bannermotd
 
 ----------------------
 Authorized access only
 ----------------------
 EOF
-systemctl restart sshd
-echo "AllowUsers sshuser" | tee -a /etc/openssh/sshd_config
 
-
+echo "AllowUsers sshuser" | tee -a /etc/ssh/sshd_config
 systemctl restart sshd
