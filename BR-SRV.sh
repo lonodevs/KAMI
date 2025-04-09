aecac7adff7187e0a23e23fa4573c4924a9c06f8
Alex <147999158+AklisTib@users.noreply.github.com>
2025-04-09 19:33:01 +0400

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -44,22 +44,23 @@ EOF
 CONFIG_FILE="/etc/openssh/sshd_config"  
 
 # Изменить SSH-порт с 22 на 2024  
-sed -i 's/^#Port 22$/Port 2024/' "$CONFIG_FILE"  
+awk -i inplace '/^#Port 22$/ { gsub(/22/, "2024"); $0 = "Port 2024" } { print }' "$CONFIG_FILE"  
 
 # Уменьшить MaxAuthTries с 6 до 2  
-sed -i 's/^#MaxAuthTries 6$/MaxAuthTries 2/' "$CONFIG_FILE"  
+awk -i inplace '/^#MaxAuthTries 6$/ { gsub(/6/, "2"); $0 = "MaxAuthTries 2" } { print }' "$CONFIG_FILE"  
 
-# Разрешить аутентификацию по паролю 
-sed -i 's/^#PasswordAuthentication yes$/PasswordAuthentication yes/' "$CONFIG_FILE"  
+# Разрешить аутентификацию по паролю  
+awk -i inplace '/^#PasswordAuthentication yes$/ { sub(/^#/, ""); print; next } { print }' "$CONFIG_FILE"  
 
-touch "$CONFIG_FILE"/bannermotd
-cat <<EOF "$CONFIG_FILE"/bannermotd
+touch "$CONFIG_FILE"/bannermotd  
+cat <<EOF > "$CONFIG_FILE"/bannermotd  
 
-----------------------
-Authorized access only
-----------------------
-EOF
-systemctl restart sshd
+----------------------  
+Authorized access only  
+----------------------  
+EOF  
+
+systemctl restart sshd  
 
 #Создание NTP
 apt-get install chrony -y 
