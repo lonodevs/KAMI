5a44b6534bfbd137abfc647679dfc3d4b880c459
Alex <147999158+AklisTib@users.noreply.github.com>
2025-06-01 19:10:53 +0400

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -19,11 +19,10 @@ awk -i inplace '/^#?MaxAuthTries[[:space:]]+6$/ {sub(/^#/,""); sub(/6/,"2"); pri
 awk -i inplace '/^#?PasswordAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
 awk -i inplace '/^#?PubkeyAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
 
-touch /etc/openssh/bannermotd  
-cat <<EOF > /etc/openssh/bannermotd 
+touch /etc/openssh/banner 
+cat <<EOF > /etc/openssh/banner
 Authorized access only  
 EOF  
-
 systemctl restart sshd  
 
 echo "default_realm = AU-TEAM.IRPO" | sudo tee -a /etc/krb5.conf
