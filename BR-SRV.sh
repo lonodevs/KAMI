bd4e8d16c13f7d03534e5d22615d6bcba29faa99
FuFFiEk <153053575+FuFFiEk@users.noreply.github.com>
2025-06-02 18:36:39 +0400

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -1,68 +1,15 @@
-#!/bin/bash
-apt-get install -y tzdata
+
 #Переименование виртуалки
 hostnamectl set-hostname br-srv.au-team.irpo;
+apt-get install -y tzdata
 timedatectl set-timezone Europe/Samara
-
-#Настройка интерфейсов и времени
-cat <<EOF > /etc/net/ifaces/ens18/options
-TYPE=eth
-DISABLED=no
-NM_CONTROLLED=no
-BOOTPROTO=static
-IPV4_CONFIG=yes
-EOF
-
-touch /etc/net/ifaces/ens18/ipv4address
-cat <<EOF > /etc/net/ifaces/ens18/ipv4address
-192.168.22.30/27
-EOF
-
-touch /etc/net/ifaces/ens18/ipv4route
-cat <<EOF > /etc/net/ifaces/ens18/ipv4route
-default via 192.168.22.1
-EOF
-
-cat <<EOF > /etc/resolv.conf
-nameserver 8.8.8.8
-nameserver 77.88.8.8
-EOF
-
-timedatectl set-timezone Europe/Samara
-systemctl restart network
-
 useradd sshuser -u 1010
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 
 touch /etc/sudoers
-cat <<EOF /etc/sudoers
+vim /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
-EOF
-
-CONFIG_FILE="/etc/openssh/sshd_config"
-
-echo "AllowUsers sshuser" | tee -a /etc/openssh/sshd_config
-awk -i inplace '/^#?Port[[:space:]]+22$/ {sub(/^#/,""); sub(/22/,"2024"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?MaxAuthTries[[:space:]]+6$/ {sub(/^#/,""); sub(/6/,"2"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PasswordAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PubkeyAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-
-touch /etc/openssh/bannermotd  
-cat <<EOF > /etc/openssh/bannermotd 
-Authorized access only  
-EOF  
-
-systemctl restart sshd  
-
-#Создание NTP
-apt-get install chrony -y 
-sed -i '3i#pool pool.ntp.org iburst' /etc/chrony.conf
-systemctl enable --now chronyd
-
-cat <<EOF >> /etc/resolv.conf 
-nameserver 8.8.8.8
-EOF
 
 #Создание Samba DC
 apt-get update && apt-get install -y task-samba-dc bind 
@@ -101,40 +48,3 @@ HQ-CLI ansilbe_host=192.168.23.66 ansible_user=sshuser ansible_password=P@ssw0rd
 ansible_python_interpreter=/usr/bin/python3
 EOF
 ansible -m ping all
-
-#Установка Docker 
-systemctl disable —now ahttpd
-apt-get install -y docker-{ce,compose}
-systemctl enable --now docker
-
-touch /home/sshuser/wiki.yaml
-cat <<EOF > /home/sshuser/wiki.yaml
-services:
-  mediawiki:
-    container_name: wiki
-    image: mediawiki
-    restart: always
-    ports:
-      - "8080:80"
-    links:
-      - db
-#    volumes:
-#      - ./LocalSettings.php:/var/www/html/LocalSettings.php
-
-  db:
-    container_name: mariadb
-    image: mariadb
-    restart: always
-    environment:
-      MARIADB_DATABASE: mediawiki
-      MARIADB_USER: wiki
-      MARIADB_PASSWORD: WikiP@ssw0rd
-      MARIADB_ROOT_PASSWORD: P@ssw0rd
-    volumes:
-      - db_data:/var/lib/mysql
-
-volumes:
-  db_data:
-EOF
-
-grep -E "Port|MaxAuthTries|PasswordAuthentication|PubkeyAuthentication" "$CONFIG_FILE"
