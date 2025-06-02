111213c51dff555e86167ca925fb9143519e2eed
FuFFiEk <153053575+FuFFiEk@users.noreply.github.com>
2025-06-02 18:38:12 +0400

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get install -y tzdata
 hostnamectl set-hostname hq-cli.au-team.irpo;
+apt-get install -y tzdata
 timedatectl set-timezone Europe/Samara
 
 useradd sshuser -u 1010
@@ -8,26 +8,11 @@ echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 
 touch /etc/sudoers
-cat <<EOF /etc/sudoers
+vim /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
-EOF
-
-CONFIG_FILE="/etc/openssh/sshd_config"
-
-echo "AllowUsers sshuser" | tee -a /etc/openssh/sshd_config
-awk -i inplace '/^#?Port[[:space:]]+22$/ {sub(/^#/,""); sub(/22/,"2024"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?MaxAuthTries[[:space:]]+6$/ {sub(/^#/,""); sub(/6/,"2"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PasswordAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PubkeyAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-
-touch /etc/openssh/banner 
-cat <<EOF > /etc/openssh/banner
-Authorized access only  
-EOF  
-systemctl restart sshd  
 
 echo "default_realm = AU-TEAM.IRPO" | sudo tee -a /etc/krb5.conf
-echo "nameserver 192.168.0.30" | sudo tee -a /etc/resolv.conf
+echo "nameserver 192.168.22.30" | sudo tee -a /etc/resolv.conf
 
 apt-get update && apt-get install -y gpupdate
 gpupdate-setup enable
