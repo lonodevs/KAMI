3758b6eec6364cbbafba8ce4b23ee640adade011
lonodevs <114346565+lonodevs@users.noreply.github.com>
2025-06-03 21:13:13 +0400

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+Имя и время
+СУС 1.1
 hostnamectl set-hostname hq-cli.au-team.irpo;
 apt-get install -y tzdata
 timedatectl set-timezone Europe/Samara
 
-useradd sshuser -u 1010
+Юзер
+useradd sshuser -u 1015
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 
@@ -11,8 +14,9 @@ touch /etc/sudoers
 vim /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
 
+Домен
 echo "default_realm = AU-TEAM.IRPO" | sudo tee -a /etc/krb5.conf
-echo "nameserver 192.168.22.30" | sudo tee -a /etc/resolv.conf
+echo "nameserver 192.168.24.14" | sudo tee -a /etc/resolv.conf
 
 apt-get update && apt-get install -y gpupdate
 gpupdate-setup enable
@@ -24,3 +28,13 @@ roleadd hq wheel
 rolelst
 apt-get install -y admx-*
 admx-msi-setup
+Зайди на суса 2.1
+
+NFS
+apt-get update && apt-get install -y nfs-{utils,clients}
+mkdir /mnt/nfs 
+chmod 777 /mnt/nfs 
+vim /etc/fstab
+192.168.18.30:/raid5/nfs /mnt/nfs nfs defaults 0 0
+mount -av
+df -h
