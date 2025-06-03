e022bd8188446534a38b7d578e62bf39cf346012
lonodevs <114346565+lonodevs@users.noreply.github.com>
2025-06-03 21:17:39 +0400

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -4,7 +4,7 @@ apt-get install -y tzdata
 timedatectl set-timezone Europe/Samara
 
 #Создание пользователя sshuser и настройка sshd конфига
-useradd sshuser -u 1010
+useradd sshuser -u 1015
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 
@@ -17,7 +17,7 @@ cat > /etc/dnsmasq.conf <<EOF
 no-resolv
 no-poll
 no-hosts
-listen-address=192.168.23.62
+listen-address=192.168.18.30
 
 server=77.88.8.8
 server=8.8.8.8
@@ -26,16 +26,21 @@ cache-size=1000
 all-servers
 no-negcache
 
-host-record=hq-rtr.au-team.irpo,192.168.23.1
-host-record=hq-srv.au-team.irpo,192.168.23.62
-host-record=hq-cli.au-team.irpo,192.168.23.66
+host-record=hq-rtr.au-team.irpo,192.168.18.1
+host-record=hq-srv.au-team.irpo,192.168.18.30
+host-record=hq-cli.au-team.irpo,192.168.18.34
 
-address=/br-rtr.au-team.irpo/192.168.22.1
-address=/br-srv.au-team.irpo/192.168.22.30
+address=/br-rtr.au-team.irpo/192.168.24.1
+address=/br-srv.au-team.irpo/192.168.24.14
 
 cname=moodle.au-team.irpo,isp.au-team.irpo
 cname=wiki.au-team.irpo,isp.au-team.irpo
 EOF
 systemctl restart dnsmasq
+
+NFS
+СУС 2.2
+
+
 
 
