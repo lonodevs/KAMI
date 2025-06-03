6b8de2d4f3ad16448eedf24b155a5f7e5bf706dc
lonodevs <114346565+lonodevs@users.noreply.github.com>
2025-06-03 21:27:20 +0400

    Update hq-rtr.sh

--- hq-rtr.sh
+++ hq-rtr.sh
@@ -5,7 +5,7 @@ ip domain-name au-team.irpo
 
 interface int0
 description "to isp"
-ip address 172.16.4.14/28
+ip address 172.16.40.14/28
 port te0
 service-instance te0/int0
 encapsulation untagged
@@ -14,10 +14,10 @@ connect port te0 service-instance te0/int0
 
 interface int1
 description "to hq-srv"
-ip address 192.168.23.1/26
+ip address 192.168.18.1/27
 port te1
 service-instance te1/int1
-encapsulation dot1q 100 exact
+encapsulation dot1q 10 exact
 rewrite pop 1
 
 interface int1
@@ -25,25 +25,25 @@ connect port te1 service-instance te1/int1
 
 interface int2
 description "to hq-cli"
-ip address 192.168.23.65/28
+ip address 192.168.18.33/28
 port te1
 service-instance te1/int2
-encapsulation dot1q 200 exact
+encapsulation dot1q 20 exact
 rewrite pop 1
 interface int2
 connect port te1 service-instance te1/int2
 
 interface int3
 description "to management"
-ip address 192.168.23.81/29
+ip address 192.168.18.48/29
 port te1
 service-instance te1/int3
-encapsulation dot1q 999 exact
+encapsulation dot1q 99 exact
 rewrite pop 1
 interface int3
 connect port te1 service-instance te1/int3
 exit
-ip route 0.0.0.0/0 172.16.4.1
+ip route 0.0.0.0/0 172.16.40.1
 username net_admin
 password P@$$word
 role admin
@@ -51,14 +51,14 @@ exit
 int tunnel.0
 ip add 172.16.0.1/30
 ip mtu 1400
-ip tunnel 172.16.4.14 172.16.5.14 mode gre
+ip tunnel 172.16.40.14 172.16.50.14 mode gre
 exit
 router ospf 1
 router-id 1.1.1.1
 network 172.16.0.0/30 area 0
-network 192.168.23.0/26 area 0
-network 192.168.23.64/28 area 0
-network 192.168.23.80/29 area 0
+network 192.168.18.0/27 area 0
+network 192.168.18.32/28 area 0
+network 192.168.18.47/29 area 0
 passive-interface default
 no passive-interface tunnel.0
 exit
@@ -75,22 +75,22 @@ ip nat inside
 int int0
 ip nat outside
 exit
-ip nat pool NAT_POOL 192.168.23.1-192.168.23.62,192.168.23.65-192.168.23.78,192.168.23.81-192.168.23.87
+ip nat pool NAT_POOL 192.168.18.1-192.168.18.30,192.168.18.33-192.168.18.45,192.168.18.48-192.168.18.53
 ip nat source dynamic inside-to-outside pool NAT_POOL overload interface int0
 ip pool cli_pool 1 
-range 192.168.23.66-192.168.23.78
+range 192.168.18.34-192.168.18.45
 exit
 dhcp-server 1
 pool cli_pool 1
 mask 28
-gateway 192.168.23.65
-dns 192.168.23.62
+gateway 192.168.18.33
+dns 192.168.18.30
 domain-name au-team.irpo
 interface int2
 dhcp-server 1
 exit
 ntp timezone utc+4
-ip nat source static tcp 192.168.23.62 2024 172.16.4.14 2024
+ip nat source static tcp 192.168.18.30 3015 172.16.40.14 3015
 security none
 exit
 wr mem
